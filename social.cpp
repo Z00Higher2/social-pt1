6ba0f8038711d66a0fd65d43839cb9dff9eeb040
Z00Higher2 <103333878+Z00Higher2@users.noreply.github.com>
2024-04-20 22:27:16 -0400

    Add files via upload

---
+++ social.cpp
@@ -0,0 +1,86 @@
+// Author: Zuhayer Tashin
+// Course: CSCI-135
+// Instructor: Tong Yi
+// Assignment: Lab 11A
+#include <iostream>
+using namespace std;
+
+class Profile {
+private:
+    string username;
+    string displayname;
+public:
+    // Profile constructor for a user (initializing 
+    // private variables username=usrn, displayname=dspn)
+    Profile(string usrn, string dspn);
+    // Default Profile constructor (username="", displayname="")
+    Profile();
+    // Return username
+    string getUsername();
+    // Return name in the format: "displayname (@username)"
+    string getFullName();
+    // Change display name
+    void setDisplayName(string dspn);
+};
+
+
+class Network {
+private:
+  static const int MAX_USERS = 20; // max number of user profiles
+  int numUsers;                    // number of registered users
+  Profile profiles[MAX_USERS];     // user profiles array:
+                                   // mapping integer ID -> Profile
+
+  // Returns user ID (index in the 'profiles' array) by their username
+  // (or -1 if username is not found)
+  int findID (string usrn);
+public:
+  // Constructor, makes an empty network (numUsers = 0)
+  Network();
+  // Attempts to sign up a new user with specified username and displayname
+  // return true if the operation was successful, otherwise return false
+  bool addUser(string usrn, string dspn);
+};
+
+
+Profile::Profile(string usrn, string dspn){
+    username = usrn;
+    displayname = dspn;
+   
+}
+
+Profile::Profile(){
+    // Initialize member variables to empty strings
+    username = "";
+    displayname = "";
+}
+
+string Profile::getUsername() {
+    return username;
+    
+}
+
+string Profile::getFullName() {
+    return displayname + " (@" + username + ")";
+}
+
+void Profile::setDisplayName(string dspn) {
+    displayname = dspn;
+}
+
+
+int main() {
+  Network nw;
+  cout << nw.addUser("mario", "Mario") << endl;     // true (1)
+  cout << nw.addUser("luigi", "Luigi") << endl;     // true (1)
+
+  cout << nw.addUser("mario", "Mario2") << endl;    // false (0)
+  cout << nw.addUser("mario 2", "Mario2") << endl;  // false (0)
+  cout << nw.addUser("mario-2", "Mario2") << endl;  // false (0)
+
+  for(int i = 2; i < 20; i++)
+      cout << nw.addUser("mario" + to_string(i), 
+                 "Mario" + to_string(i)) << endl;   // true (1)
+
+  cout << nw.addUser("yoshi", "Yoshi") << endl;     // false (0)
+}
